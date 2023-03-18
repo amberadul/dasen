1c3c81ae4b71c914dd834aa07fca7c31eddafd5b
amberadul <128192812+amberadul@users.noreply.github.com>
2023-03-18 12:16:11 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -6,5 +6,6 @@ apt-get update
 
 apt-get install qemu-system-x86 -y
 
-qemu-system-x86_64 -hda w7x86.img -m 16G -smp cores=8 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
-clear
+qemu-system-x86_64 -hda w7x86.img -m 16G -smp cores=8 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic 
+
+
